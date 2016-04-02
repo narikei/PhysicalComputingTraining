641a0ff0175ce473a37d4119e435027142bf3c85
なりたけいすけ <narita_keisuke@cyberagent.co.jp>
2016-04-02 18:31:00 +0900

    no message

---
+++ FloppyJump/flip/flip.ino
@@ -0,0 +1,41 @@
+#include <Keyboard.h>
+
+const int analogPressPin = 1;
+const int soundPin = 10;
+
+boolean direction = true;
+int values[5] = {1024, 1024, 1024, 1024, 1024};
+
+void setup(){
+  Serial.begin(9600);  // シリアル通信速度
+  Keyboard.begin(); // キーボード開始
+  pinMode(soundPin, OUTPUT);
+}
+
+void loop(){
+  for (int i = 0; i < 5; i++) {
+    values[i] = values[i+1];
+  }
+  values[4] = analogRead(analogPressPin);
+
+  // 平均値
+  double value = 0;
+  for (int i = 0; i < 5; i++) {
+    value = value + values[i];
+  }
+  value = value / 5;
+
+  Serial.println("+");
+  Serial.println(value);
+
+  // ジャンブ
+  if (value < 25) {
+    Keyboard.press(' ');
+    tone(soundPin, 440, 100);
+    delay(100);
+    Keyboard.releaseAll();
+  }
+
+  delay(50);
+}
+
